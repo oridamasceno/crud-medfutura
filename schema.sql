86e113072a6fec0f27c31e67c39f13428528026a
oridamasceno <ori1.contato@gmail.com>
2024-09-01 15:52:53 -0300

    WF: adding schema file

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE PessoaDB;
+
+USE PessoaDB;
+
+CREATE TABLE Pessoas (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    apelido VARCHAR(32) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    nascimento DATE NOT NULL,
+    stack NVARCHAR(32) ARRAY
+);
