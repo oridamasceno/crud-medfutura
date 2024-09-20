b885aee6571074a4d02b2aee76ae94148c5b68e7
ori <142668608+oridamasceno@users.noreply.github.com>
2024-09-20 11:23:19 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,6 +6,5 @@ CREATE TABLE Pessoas (
     id INT PRIMARY KEY IDENTITY(1,1),
     apelido VARCHAR(32) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
-    nascimento DATE NOT NULL,
-    stack NVARCHAR(32) ARRAY
+    nascimento DATE NOT NULL
 );
